bfd5aedd81737c69dfffb20976e18e959b49c7e3
Dan Patey <danpatey@danpatey.com>
2016-07-04 00:44:55 -0700

    inital commit for UICollectionView update

---
+++ Photorama/PhotoDataSource.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Dan Patey on 7/4/16.
+//  Copyright © 2016 Dan Patey. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath)
+        
+        return cell
+    }
+}
