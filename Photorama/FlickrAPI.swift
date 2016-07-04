85b7e8c1f00e3c9616ff59ffcb17c3a6061380d1
Dan Patey <danpatey@danpatey.com>
2016-07-03 23:49:51 -0700

    Error handling for days

--- Photorama/FlickrAPI.swift
+++ Photorama/FlickrAPI.swift
@@ -12,10 +12,25 @@ enum Method: String {
     case RecentPhotos = "flickr.photos.getRecent"
 }
 
+enum PhotosResult {
+    case Success([Photo])
+    case Failure(ErrorType)
+}
+
+enum FlickrError: ErrorType {
+    case InvalidJSONData
+}
+
 struct FlickrAPI {
     
     private static let baseURLString = "https://api.flickr.com/services/rest"
-    private static let APIKey = "a​6​d​8​1​9​4​9​9​1​3​1​0​7​1​f​1​5​8​f​d​7​4​0​8​6​0​a​5​a​8​8"
+    private static let APIKey = "a6d819499131071f158fd740860a5a88"
+    
+    private static let dateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter
+    }()
     
     private static func flickrURL(method method: Method, parameters: [String:String]?) -> NSURL {
         
@@ -45,5 +60,47 @@ struct FlickrAPI {
     
     static func recentPhotosURL() -> NSURL {
         return flickrURL(method: .RecentPhotos, parameters: ["extras": "url_h,date_taken"])
+    }
+    
+    static func photosFromJSONData(data: NSData) -> PhotosResult {
+        do {
+            let jsonObject: AnyObject = try NSJSONSerialization.JSONObjectWithData(data, options: [])
+            
+            guard let jsonDictionary = jsonObject as? [NSObject:AnyObject],
+            photos = jsonDictionary["photos"] as? [String:AnyObject],
+                photosArray = photos["photo"] as? [[String:AnyObject]] else {
+                    // The JSON structure doesn't match our expectations
+                    return .Failure(FlickrError.InvalidJSONData)
+            }
+            
+            var finalPhotos = [Photo]()
+            for photoJSON in photosArray {
+                if let photo = photoFromJSONObject(photoJSON) {
+                    finalPhotos.append(photo)
+                }
+            }
+            
+            if finalPhotos.count == 0 && photosArray.count > 0 {
+                // We weren't able to parse any of the photos maybe the JSON format changed
+                return .Failure(FlickrError.InvalidJSONData)
+            }
+            
+            return .Success(finalPhotos)
+        } catch let error {
+            return .Failure(error)
+        }
+    }
+    
+    private static func photoFromJSONObject(json: [String : AnyObject]) -> Photo? {
+        guard let photoID = json["id"] as? String,
+        title = json["title"] as? String,
+        dateString = json["datetaken"] as? String,
+        photoURLString = json["url_h"] as? String,
+        url = NSURL(string: photoURLString),
+            dateTaken = dateFormatter.dateFromString(dateString) else {
+                // Don't have enough information to construct a Photo
+                return nil
+        }
+        return Photo(title: title, photoID: photoID, remoteURL: url, dateTaken: dateTaken)
     }
 }
