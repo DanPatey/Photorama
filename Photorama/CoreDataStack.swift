9496e4833df718a34bb28e5eff30f0d2f79bfbea
Dan Patey <danpatey@danpatey.com>
2016-07-04 02:08:19 -0700

    Resolving MySQLite error

--- Photorama/CoreDataStack.swift
+++ Photorama/CoreDataStack.swift
@@ -24,11 +24,16 @@ class CoreDataStack {
     }()
     
     private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        
         var coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        
         let pathComponent = "\(self.managedObjectModelName).sqlite"
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(pathComponent)
         
-        let store = try! coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: nil, options: nil)
+        let store = try! coordinator.addPersistentStoreWithType(NSSQLiteStoreType,
+                                                                configuration: nil,
+                                                                URL: url,
+                                                                options: nil)
         
         return coordinator
     }()
