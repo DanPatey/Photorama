e4a0da700b016760816e82bdc596c1871f66a44e
Dan Patey <danpatey@danpatey.com>
2016-07-03 22:01:02 -0700

    Inital commit

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Dan Patey on 7/3/16.
+//  Copyright © 2016 Dan Patey. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
