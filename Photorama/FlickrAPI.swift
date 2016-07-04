cffc1acedb9ed02f3ba4f512089f23d6c5778042
Dan Patey <danpatey@danpatey.com>
2016-07-03 22:39:33 -0700

    Finishing off the last of what I need for the flickr url request

--- Photorama/FlickrAPI.swift
+++ Photorama/FlickrAPI.swift
@@ -15,11 +15,22 @@ enum Method: String {
 struct FlickrAPI {
     
     private static let baseURLString = "https://api.flickr.com/services/rest"
+    private static let APIKey = "a​6​d​8​1​9​4​9​9​1​3​1​0​7​1​f​1​5​8​f​d​7​4​0​8​6​0​a​5​a​8​8"
     
     private static func flickrURL(method method: Method, parameters: [String:String]?) -> NSURL {
         
         let components = NSURLComponents(string: baseURLString)!
         var queryItems = [NSURLQueryItem]()
+        
+        let baseParams = ["method": method.rawValue,
+                          "format": "json",
+                          "nojsoncallback": "1",
+                          "api_key": APIKey]
+        
+        for (key, value) in baseParams {
+            let item = NSURLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
         
         if let additionalParams = parameters {
             for (key, value) in additionalParams {
